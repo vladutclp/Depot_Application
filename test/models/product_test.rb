9b7d59341dc279f92940d309c434912bbf2f613d
Claps <vladut.clapou@gmail.com>
2019-12-29 10:58:06 +0200

    Unit testing and validation.

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,71 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+	fixtures :products
+
+	test 'product attributes must not be empty' do
+		product = Product.new
+		assert product.invalid?
+		assert product.errors[:title].any?
+		assert product.errors[:description].any?
+		assert product.errors[:image_url].any?
+		assert product.errors[:price].any?		
+	end
+
+	test 'price must be positive' do
+		product = Product.new(title: 	   'My Book Title',
+							  description: 'yyy',
+							  image_url:   'asd.jpg')
+
+		product.price = -1
+		assert product.invalid?
+		assert_equal ['must be greater than or equal to 0.01'], product.errors[:price]
+
+		product.price = 0
+		assert product.invalid?
+		assert_equal ['must be greater than or equal to 0.01'], product.errors[:price]
+
+		product.price = 1
+		assert product.valid?
+	end		
+
+	def new_product(image_url)
+		product = Product.new(title: 	   'My Book Title',
+							  description: 'yyy',
+							  image_url:   image_url,
+							  price: 	   1)
+	end
+
+
+	test "image url" do
+		ok =  %w{ fred.gif fred.png freg.jpg FRED.JPG FRED.Jpg http://a.b.c/x/y/z/fred.gif }
+		bad = %w{ fred.doc fred.gif/more fred.gif.more }
+
+		ok.each do |image_url|
+			assert new_product(image_url).valid?, "#{image_url} shouldn't be valid"
+		end
+
+		bad.each do |image_url|
+			assert new_product(image_url).invalid?, "#{image_url} shouldn't be invalid"
+		end
+	end		
+
+	test 'product is not valid without an unique name' do
+		product = Product.new(title: 		products(:ruby).title,
+							 description: 	'y',
+							 image_url: 	"fred.gif",
+							 price: 		1)
+		assert product.invalid?
+		assert_equal ['has already been taken'], product.errors[:title]
+	end
+
+	test 'price must be higher than 30$' do
+		product = products(:one)
+
+		assert product.price<30, "the actual price is #{product.price}"
+	end
+
 end
+
+
