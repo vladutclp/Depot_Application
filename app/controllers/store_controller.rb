1181317bedbf57dd4464cf5554c2dc3dfc4162e6
Claps <vladut.clapou@gmail.com>
2020-01-11 21:46:13 +0200

    Task D completed!

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,7 +1,16 @@
 class StoreController < ApplicationController
 
+
+
   def index
+  	
   	@products = Product.order(:title)
+  	#Shows how many times the store maine page has been accessed
+  	if session[:counter].nil?
+		session[:counter] = 0
+	else
+		session[:counter] = session[:counter] + 1
+	end
   end
   
 end
