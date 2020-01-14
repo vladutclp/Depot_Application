36381f607c95404b054cae6f2225e56f4de1327b
Claps <vladut.clapou@gmail.com>
2020-01-14 20:14:32 +0200

    Task E error

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,3 +1,14 @@
 class Cart < ApplicationRecord
 	has_many :line_items, dependent: :destroy
+
+	def add_product(product)
+		current_item = line_items.find_by(product_id: product.id) #find_by returns either an existing LineItem or nil
+
+		if current_item
+			current_item.quantity += 1
+		else
+			current_item = line_items.build(product_id: product.id)
+		end
+	end
+
 end
